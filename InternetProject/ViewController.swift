53cd010d36388830d72d0836e1ed54e6b667a0ac
Popcornblaster <lucinablaster@gmail.com>
2018-12-12 13:50:33 -0700

    Initial Commit

---
+++ InternetProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InternetProject
+//
+//  Created by Baugh, Tyler on 12/12/18.
+//  Copyright © 2018 Baugh, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
